077ff3dabff52713b8b3e062a6c355f0809d072e
Kearen <kearen03@gmail.com>
2025-10-27 09:27:33 -0700

    Empty conversion methods created

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class TemperatureConverter {
+public class TemperatureConverter {
+
+    public static void main(String[] args) {
+
+    }
+
+    public double celsiusToFahrenheit(double celsius) {
+        return 0;
+    }
+
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return 0;
+    }
+
+    public double celciusToKelvin(double celsius) {
+        return 0;
+    }
 }
